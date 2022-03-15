e0070bfb134671408d1229da86f1231d8461d25c
최영찬 <gks54410@gmail.com>
2022-03-15 17:33:50 +0900

    Initial Commit

---
+++ src/main/java/jpa/springDataJpaStudy/SpringDataJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package jpa.springDataJpaStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa/springDataJpaStudy/SpringDataJpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.springDataJpaStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
