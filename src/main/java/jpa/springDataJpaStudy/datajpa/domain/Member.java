88c8db1c5094ec818623f2352cfbfe1275c39f81
최영찬 <gks54410@gmail.com>
2022-03-16 15:09:39 +0900

    Feat: 회원 이름을 파라미터로 받는 생성자 추가

--- src/main/java/jpa/springDataJpaStudy/datajpa/domain/Member.java
+++ src/main/java/jpa/springDataJpaStudy/datajpa/domain/Member.java
@@ -20,6 +20,10 @@ public class Member {
     @JoinColumn(name = "team_id")
     private Team team;
 
+    public Member(String username) {
+        this.username = username;
+    }
+
     public Member(String username, int age, Team team) {
         this.username = username;
         this.age = age;
