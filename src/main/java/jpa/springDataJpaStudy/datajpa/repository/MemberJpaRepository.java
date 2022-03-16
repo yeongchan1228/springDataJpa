543e99756d6cef5e8a98a43f14747f73d3bf373c
최영찬 <gks54410@gmail.com>
2022-03-16 15:11:37 +0900

    Feat: 프로젝트 회원 도메인의 Jpa를 활용한 Repository 추가
    - 저장
    - 삭제
    - 회원 조회
    - 회원 count 조회

---
+++ src/main/java/jpa/springDataJpaStudy/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,44 @@
+package jpa.springDataJpaStudy.datajpa.repository;
+
+import jpa.springDataJpaStudy.datajpa.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberJpaRepository {
+
+    private final EntityManager em;
+
+    public Member save(Member member){
+        em.persist(member);
+
+        return member;
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Optional<Member > findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
+}
