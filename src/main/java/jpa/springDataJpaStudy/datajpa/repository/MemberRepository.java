bc7b27f5e6f8b2b1d488dfd81fd098d56f12c694
최영찬 <gks54410@gmail.com>
2022-03-16 15:55:52 +0900

    Feat: 회원 이름 조회, 전체 조회, 회원 이름과 나이 조건을 통한 조회, Limit 조회 추가
    SpringDataJpa의 메서드 이름을 활용한 쿼리 생성 사용

--- src/main/java/jpa/springDataJpaStudy/datajpa/repository/MemberRepository.java
+++ src/main/java/jpa/springDataJpaStudy/datajpa/repository/MemberRepository.java
@@ -3,5 +3,17 @@ package jpa.springDataJpaStudy.datajpa.repository;
 import jpa.springDataJpaStudy.datajpa.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username); // 쿼리 메서드 기능 -> 이름 조회
+    List<Member> findXXBy(String username); // 쿼리 메서드 기능 -> 전체 조회
+    List<Member> findTop3XXBy(String username); // 쿼리 메서드 기능 -> Limit 조회 -> limit3
+
+    /**
+     * 쿼리 메서드 기능
+     * 메서드 이름을 가지고 쿼리 생성
+     */
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
