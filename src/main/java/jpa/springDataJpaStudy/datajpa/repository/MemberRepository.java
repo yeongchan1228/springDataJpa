ecd25e933ac73734205be9cb84c898d3a29f1466
최영찬 <gks54410@gmail.com>
2022-03-17 10:31:19 +0900

    Feat: 회원 이름을 통한 조회(컬렉션 파라미터 바인딩) 추가

--- src/main/java/jpa/springDataJpaStudy/datajpa/repository/MemberRepository.java
+++ src/main/java/jpa/springDataJpaStudy/datajpa/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import javax.persistence.NamedQuery;
+import java.util.Collection;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -44,4 +45,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select new jpa.springDataJpaStudy.datajpa.repository.dto" +
             ".MemberDto(m.id, m.username, t.name) from Member m join m.team t")
     List<MemberDto> findMemberDto();
+
+    /**
+     * 컬렉션 파라미터 바인딩
+     */
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param(value = "names") Collection<String> names);
+
 }
