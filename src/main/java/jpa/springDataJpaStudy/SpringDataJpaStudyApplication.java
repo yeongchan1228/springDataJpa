c60c1cb3b08a914f83cd27daf9dc14a15ccb75ad
최영찬 <gks54410@gmail.com>
2022-03-20 00:03:48 +0900

    Feat: 스프링 데이터 JPA 등록자, 수정자 처리를 위한 메서드 추가

--- src/main/java/jpa/springDataJpaStudy/SpringDataJpaStudyApplication.java
+++ src/main/java/jpa/springDataJpaStudy/SpringDataJpaStudyApplication.java
@@ -2,7 +2,12 @@ package jpa.springDataJpaStudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @EnableJpaAuditing // Auditing을 위한 설정
 @SpringBootApplication
@@ -12,4 +17,9 @@ public class SpringDataJpaStudyApplication {
 		SpringApplication.run(SpringDataJpaStudyApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider(){ // 등록자, 수정자를 넣기 위해 필요한 메서드
+		// 스프링 시큐리티를 사용하면 거기서 세션에서 정보를 꺼내와 넣어줘야 한다.
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
