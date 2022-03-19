6115e31b29b6884dcc42b10bddc3f8b348369233
최영찬 <gks54410@gmail.com>
2022-03-19 18:52:08 +0900

    Feat: NamedEntityGraph 추가

--- src/main/java/jpa/springDataJpaStudy/datajpa/domain/Member.java
+++ src/main/java/jpa/springDataJpaStudy/datajpa/domain/Member.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 @ToString(of = {"id", "username", "age"})
 @NamedQuery(name = "Member.findByUsername",
                 query = "select m from Member m where m.username = :username")
+@NamedEntityGraph(name = "Member.all", attributeNodes = @NamedAttributeNode(value = "team"))
 public class Member {
 
     @Id @GeneratedValue
