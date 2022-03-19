ac0b328723025f25db779600e66861f8f8e40b31
최영찬 <gks54410@gmail.com>
2022-03-19 23:50:36 +0900

    Test: JPA 기반 회원 등록일, 수정일 기능 테스트

--- src/test/java/jpa/springDataJpaStudy/datajpa/domain/MemberTest.java
+++ src/test/java/jpa/springDataJpaStudy/datajpa/domain/MemberTest.java
@@ -1,6 +1,8 @@
 package jpa.springDataJpaStudy.datajpa.domain;
 
+import jpa.springDataJpaStudy.datajpa.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +11,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +22,8 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void testEntity() throws Exception {
@@ -50,5 +55,25 @@ class MemberTest {
             System.out.println("result.Team = " + result.getTeam());
         }
 
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        // given
+        Member member = new Member("memberA");
+        Member saveMember = memberRepository.save(member);// @PrePersist 실행
+
+        Thread.sleep(100);
+        saveMember.setUsername("memberB");
+
+        em.flush(); // @PreUpdate 실행
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        // then
+        System.out.println(findMember.getCreateDate());
+        System.out.println(findMember.getUpdateDate());
     }
 }
