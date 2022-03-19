db633c92fa1ad37b6edfe90bb728aed3e5404465
최영찬 <gks54410@gmail.com>
2022-03-19 23:51:04 +0900

    Feat: 스프링 데이터 JPA Auditing을 위한 어노테이션 추가

--- src/main/java/jpa/springDataJpaStudy/SpringDataJpaStudyApplication.java
+++ src/main/java/jpa/springDataJpaStudy/SpringDataJpaStudyApplication.java
@@ -2,7 +2,9 @@ package jpa.springDataJpaStudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // Auditing을 위한 설정
 @SpringBootApplication
 public class SpringDataJpaStudyApplication {
 
