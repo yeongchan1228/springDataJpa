172b014848dcea9eda670e11cffc9bd7a9095ada
최영찬 <gks54410@gmail.com>
2022-03-21 11:04:32 +0900

    Feat: 숫자형 객체 타입의 PK가 어떻게 할당되어 DB에 들어가는지 확인하기 위한 클래스 추가

---
+++ src/main/java/jpa/springDataJpaStudy/datajpa/domain/Item.java
@@ -0,0 +1,12 @@
+package jpa.springDataJpaStudy.datajpa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id @GeneratedValue
+    private Long id;
+}
