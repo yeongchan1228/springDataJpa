715ec1e4cfc23d5caee83acd6774ede543f7360c
최영찬 <gks54410@gmail.com>
2022-03-21 11:18:28 +0900

    Feat: PK를 문자열로 구현

--- src/main/java/jpa/springDataJpaStudy/datajpa/domain/Item.java
+++ src/main/java/jpa/springDataJpaStudy/datajpa/domain/Item.java
@@ -1,12 +1,44 @@
 package jpa.springDataJpaStudy.datajpa.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-public class Item {
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
 
-    @Id @GeneratedValue
-    private Long id;
+//    @Id @GeneratedValue
+//    private Long id;
+
+    @Id
+    private String id; // Pk가 문자형 객체에 자신이 값을 할당할 때, 스프링 데이터 JPA의 save를 오버라이드 해야 한다.
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        createdDate = LocalDateTime.now();
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
 }
