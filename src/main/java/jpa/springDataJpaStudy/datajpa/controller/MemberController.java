799953a53110a68ce82daa5eb13bdb465252c384
최영찬 <gks54410@gmail.com>
2022-03-21 18:55:17 +0900

    Fix: @PostConstruct 주석 처리, db 테이블 없을 시 오류 방지

--- src/main/java/jpa/springDataJpaStudy/datajpa/controller/MemberController.java
+++ src/main/java/jpa/springDataJpaStudy/datajpa/controller/MemberController.java
@@ -47,10 +47,10 @@ public class MemberController {
         return map;
     }
 
-    @PostConstruct
-    public void init(){
-        for (int i = 0; i < 100; i++) {
-            memberRepository.save(new Member("member" + i));
-        }
-    }
+//    @PostConstruct
+//    public void init(){
+//        for (int i = 0; i < 100; i++) {
+//            memberRepository.save(new Member("member" + i));
+//        }
+//    }
 }
